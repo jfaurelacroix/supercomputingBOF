33ebb2918a3f356871f80fc79a2ec24e77b0b94b
Julie Faure-Lacroix <jfaurelacroix@users.noreply.github.com>
2026-02-16 13:45:09 -0500

    edited csv name and dates in r file
    changed all 2025 to 2026

--- analysis_file.r
+++ analysis_file.r
@@ -8,5 +8,5 @@ print("This is going to be executed though")
 
 # Then, we'll load the data file
 
-data2025 <- read.csv("./data_sca_2025.csv")
-print(data2025)
+data2026 <- read.csv("./data_sca_2026.csv")
+print(data2026)
