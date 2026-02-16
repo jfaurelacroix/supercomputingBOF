33676f077dd9ffe5ddc396445e3c22ba4cde178f
Julie Faure-Lacroix <jfaurelacroix@users.noreply.github.com>
2026-02-16 12:16:04 -0500

    Create analysis_file.r
    Created an R file that will be used for the analysis and visualization. Added some context and comments, as well as a first task.

---
+++ analysis_file.r
@@ -0,0 +1,10 @@
+# This is a comment in R. Do not confuse it with a markdown title. 
+# Comments aren't going to be executed when running the code
+print("This is going to be executed though")
+
+# We'll edit this file to run the analysis.
+# Your first job is to remove all comments, write a new series of comments to describe the
+# purpose of this file, as well as our names and email addresses for future reference
+
+# Then, we'll load the data file
+
