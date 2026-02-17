7e03d2c366e22596b65c07c0570b6034b4edf076
Julie Faure-Lacroix <jfaurelacroix@users.noreply.github.com>
2026-02-17 09:40:06 -0500

    added instructions to analysis_file.r
    added instructions to set working directory manually

--- analysis_file.r
+++ analysis_file.r
@@ -7,6 +7,8 @@ print("This is going to be executed though")
 # purpose of this file, as well as our names and email addresses for future reference
 
 # Then, we'll load the data file
-
+# Because we'll both be working from our respective computers, set your working
+# directory from the Rstudio menu 
+# session -> set working directory -> to source file location
 data2026 <- read.csv("./data_sca_2026.csv")
 print(data2026)
