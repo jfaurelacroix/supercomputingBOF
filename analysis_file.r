2e12d94415f6f5fe0e4653c0d69d13e114d069e2
Eugene Low <eugene@nscc.sg>
2026-04-07 16:49:48 +0800

    Update analysis_file.r
    - Attempts at R to analyse 'fake_hpc_events' data
    - Homework 1 - discovered 'length' code for number total
    - Homework 2 - unsure of code; attempts pending

--- analysis_file.r
+++ analysis_file.r
@@ -1,5 +1,7 @@
-# This is a comment in R. Do not confuse it with a markdown title. 
-# Comments aren't going to be executed when running the code
+# This R session has two parts - R training and data analysis. 
+# The objective of the R training session is the familiarization with R Studio, its functions and code.
+# The objective of the data analysis is to apply what has been learned in R Studio to analysis data on reporting funding metrics from around the world and present this results at relevant platforms.
+# The project is authored by Julie Faure-Lacroix (CalculQuebec) and Eugene Low (NSCC)
 print("This is going to be executed though")
 
 # We'll edit this file to run the analysis.
@@ -27,5 +29,32 @@ unique(fake_hpc_events$funding_guaranteed)
 names(fake_hpc_events)
 # tapply to select certain variables and FUN (function) unique
 tapply(fake_hpc_events$country,fake_hpc_events$event,FUN = unique)
+
+tapply(fake_hpc_events$event,fake_hpc_events$country,FUN = unique)
 # ? for query about waht the function is
 ?tapply
+
+# use tapply to give the number of submitted responses per event
+fake_hpc_events <- read.csv("./fake_hpc_events.csv")
+str(fake_hpc_events)
+# I could use FUN=unique but what if i tried number / numeric? 
+tapply(fake_hpc_events$event,FUN = numeric) 
+# no dice, FUN=unique it is  
+tapply(fake_hpc_events$event,FUN = unique.numeric_version)
+# no dice too; even tried unique.numeric... and some of the others below
+unique(fake_hpc_events$event,sum)
+tapply(fake_hpc_events$event,FUN = integer)
+tapply(fake_hpc_events$event,FUN = NULL,sum(unique))
+str(fake_hpc_events)
+tapply(fake_hpc_events$event,FUN = length) 
+# had to find out from Google to include X, INDEX and function but why would you need INDEX if X is the same?
+tapply(fake_hpc_events$event,fake_hpc_events$event, length)
+
+# use tapply to give the number of submitted responses per year
+# below is numbers by date but not by year
+tapply(fake_hpc_events$date,fake_hpc_events$date, length)
+
+# attempts at sorting number of responses by year
+tapply(fake_hpc_events$date,fake_hpc_events$date,FUN = length)
+tapply(fake_hpc_events$date,length, FUN = range.Date(2026, 2025))
+tapply(fake_hpc_events$date,fake_hpc_events$date,FUN = %Y)
